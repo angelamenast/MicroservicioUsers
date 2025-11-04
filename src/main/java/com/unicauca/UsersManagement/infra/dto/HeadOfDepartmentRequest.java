9dda94d77cd6d664d6ce040a2a1aeb8913f01b26
LuisaFerPas <luisafp@unicauca.edu.co>
2025-11-04 00:27:53 -0500

    Ultima Version ProjectManagement

--- src/main/java/com/unicauca/UsersManagement/infra/dto/HeadOfDepartmentRequest.java
+++ src/main/java/com/unicauca/UsersManagement/infra/dto/HeadOfDepartmentRequest.java
@@ -1,17 +1,13 @@
-package com.unicauca.usersmanagement.infra.dto;
+package com.unicauca.UsersManagement.infra.dto;
 
 import lombok.Getter;
 import lombok.Setter;
 
-public class HeadOfDepartmentRequest {
+public class HeadOfDepartmentEvent {
     @Getter @Setter
     private String name;
     @Getter @Setter
     private String lastName;
-    @Getter @Setter
-    private String phoneNumber;
-    @Getter @Setter
-    private String program;
     @Getter @Setter
     private UserRequest userRequest;
     @Getter @Setter
